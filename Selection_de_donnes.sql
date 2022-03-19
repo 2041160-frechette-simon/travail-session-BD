75cfd236a68c2c74898bd639ff1ecf7756a3de14
2135081-Provencher-Clement <2135081@carrefour.cegepvicto.ca>
2022-03-19 11:29:49 -0400

    Numéro J fait

--- Selection_de_donnes.sql
+++ Selection_de_donnes.sql
@@ -1,12 +1,11 @@
-# -- L’expédition des « girafes triomphantes » ont vaincu lesmonstres de la salle « salle de repos» et 
-# -- récolte maintenant le trésor de la salle, calculez la valeur amassez.
-SELECT sum(valeur) FROM Expedition
+# -- J
+SELECT quantite * valeur AS valeur_salle_objets FROM Expedition
 	INNER JOIN Visite_salle ON expedition = id_expedition
     INNER JOIN Salle ON Visite_salle.salle = id_salle
     INNER JOIN Coffre_tresor ON Coffre_tresor.salle = id_salle
     INNER JOIN Ligne_coffre ON coffre = id_coffre_tresor
     INNER JOIN Objet ON objet = id_objet
-    WHERE nom = 'girafes triomphantes' AND fonction = 'salle de repos';
+    WHERE nom_equipe = 'girafes triomphantes' AND fonction = 'salle de repos';
 
 # -- K   
 SELECT code_secret FROM Coffre_tresor
