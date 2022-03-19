ce2f321102a1513c6a2e0b39eb43340fb6403b97
2135081-Provencher-Clement <2135081@carrefour.cegepvicto.ca>
2022-03-19 11:49:59 -0400

    Terminé le numéro L et M, arrangé le J et le K

--- Selection_de_donnes.sql
+++ Selection_de_donnes.sql
@@ -1,5 +1,5 @@
 # -- J
-SELECT quantite * valeur AS valeur_salle_objets FROM Expedition
+SELECT sum(quantite * valeur) AS valeur_salle_objets FROM Expedition
 	INNER JOIN Visite_salle ON expedition = id_expedition
     INNER JOIN Salle ON Visite_salle.salle = id_salle
     INNER JOIN Coffre_tresor ON Coffre_tresor.salle = id_salle
@@ -9,11 +9,24 @@ SELECT quantite * valeur AS valeur_salle_objets FROM Expedition
 
 # -- K   
 SELECT code_secret FROM Coffre_tresor
-	WHERE code_secret RLIKE '^\\d{0,5}$' 
+	WHERE code_secret RLIKE '^\\w{0,5}$' 
     OR code_secret RLIKE '^\\d+$'
     OR code_secret RLIKE 'salle';
     
+# -- L
 SELECT Salle.* FROM Salle
 	INNER JOIN Coffre_tresor ON Coffre_tresor.salle = id_salle
     INNER JOIN Ligne_coffre ON coffre = id_coffre_tresor
-    INNER JOIN Objet ON objet = id_objet;
+    INNER JOIN Objet ON objet = id_objet
+    GROUP BY Salle
+    HAVING sum(valeur * quantite) > 2000 AND salle_suivante IS NOT NULL;
+    
+# -- M
+SELECT nom, sum(datediff(fin_affectation, debut_affectation)) AS temps_affectation_total FROM Monstre
+	INNER JOIN Affectation_salle ON monstre = id_monstre
+    GROUP BY nom
+    ORDER BY temps_affectation_total DESC
+    LIMIT 1;
+    
+# -- N
+# --TODO
